0d598c109be7ed37e51c45495f49db7e87e49415
Avitej <82682098+Avitej@users.noreply.github.com>
2023-02-12 16:33:24 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -41,6 +41,19 @@ poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 
+function preload() {
+	ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+	missed = loadSound("missed.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function restart() {
+  pcscore = 0;
+  playerscore = 0;
+  loop()
+}
+
 function modelLoaded() {
   console.log('PoseNet Is Initialized');
 }
@@ -168,11 +181,11 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
-    
+    ball_touch_paddel.play();
   }
   else{
     pcscore++;
-    
+    missed.play();
     reset();
     navigator.vibrate(100);
   }
@@ -185,7 +198,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25);
     text("Game Over!",width/2,height/2);
-    text("Reload the page!",width/2,height/2+30)
+    text("pr5ess restart to play again!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
  }
